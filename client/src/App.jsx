24657713c2b50f27a5877dd11a40747f6e9143c3
azeemudeen <azeemdheen7@gmail.com>
2023-07-31 00:49:57 +0530

    fixed route bug & add dummy data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,42 +12,57 @@ export const AppContext = createContext();
 function App() {
   const [records, setRecords] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const dummyRecords = [
+    { title: "Lunch", desc: "-", amount: 80, date: "13/03/2023", category: "FOOD", type: "Debit" },
+    { title: "Book", desc: "-", amount: 30, date: "13/03/2023", category: "STATIONARY", type: "Debit" },
+    { title: "Salary", desc: "-", amount: 1080, date: "13/03/2023", category: "WORK", type: "Credit" },
+    { title: "Sales", desc: "-", amount: 180, date: "13/03/2023", category: "WORK", type: "Credit" }
+  ]
+
+  const dummyCat = ["WORK","STATIONARY","FOOD"];
 
   useEffect(() => {
-    const rec = localStorage.getItem("records");
-    const cat = localStorage.getItem("categories");
-    if (rec) {
-      setRecords(JSON.parse(rec));
-    }
-    if (cat) {
-      setCategories(JSON.parse(cat));
-    }
+      const rec = localStorage.getItem("records");
+      const cat = localStorage.getItem("categories");
+      if (rec) {
+        setRecords(JSON.parse(rec));
+      }
+      if (cat) {
+        setCategories(JSON.parse(cat));
+      }
+      setRecords((rec) => rec.concat(dummyRecords));
+      setCategories((cat) => cat.concat(dummyCat));
+      setIsLoading(false);
   }, []);
 
   return (
     <>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={
-            <AppContext.Provider value={{ records, setRecords, categories }}>
-              <Home />
-            </AppContext.Provider>
-          } />
-          <Route path="/categories" element={
-            <AppContext.Provider value={{ categories, setCategories }}>
-              <Categories />
-            </AppContext.Provider>
-          }
-          />
-          <Route path="/reports" element={
-            <AppContext.Provider value={{ records, categories }}>
-              <Reports />
-            </AppContext.Provider>
-          } />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      { !isLoading && (
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={
+              <AppContext.Provider value={{ records, setRecords, categories }}>
+                <Home />
+              </AppContext.Provider>
+            } />
+            <Route path="/categories" element={
+              <AppContext.Provider value={{ categories, setCategories }}>
+                <Categories />
+              </AppContext.Provider>
+            }
+            />
+            <Route path="/reports" element={
+              <AppContext.Provider value={{ records, categories }}>
+                <Reports />
+              </AppContext.Provider>
+            } />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      )}
     </>
   )
 }
